2b93c259406a7e9af8cf4f1fcb4b430e5f5b219f
coolseaweed <jeellato@gmail.com>
2020-11-11 01:20:55 +0900

    :D

--- run.sh
+++ run.sh
@@ -5,22 +5,22 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
-#
-#function bashAndVimSetting() {
-#    # bash, vim 설정파일 복사
-#    cp ./.bashrc ~/;
-#    cp ./.bash_profile ~/;
-#    cp ./.vimrc ~/;
-#
-#    # vim vundle 설치
-#    rm -rf ~/.vim/bundle
-#    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#    # vundle plugins 설치
-#    vim +PluginInstall +qall
-#}
-#
-#read -p "This may overwrite existing your current setting files in home directory. Are you sure? (y/n) " -n 1;
-#echo "";
-#if [[ $REPLY =~ ^[Yy]$ ]]; then
-#	bashAndVimSetting;
-#fi; 
+
+function envSetup() {
+    # bash, vim 설정파일 복사
+    cp ./.bashrc ~/;
+    #cp ./.bash_profile ~/;
+    cp ./.vimrc ~/;
+
+    # vim vundle 설치
+    rm -rf ~/.vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    # vundle plugins 설치
+    vim +PluginInstall +qall
+}
+
+read -p "This may overwrite existing your current setting files in home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	envSetup;
+fi; 
