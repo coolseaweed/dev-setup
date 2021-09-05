067d423529dedb5013fff0bbb73e43b3ab1c53d6
coolseaweed <jeellato@gmail.com>
2021-09-05 16:55:24 +0900

    debug conda init

--- miniconda/install_conda.sh
+++ miniconda/install_conda.sh
@@ -2,12 +2,12 @@
 
 
 OS="linux" # centos macos windows
-PREFIX=$HOME/ENV/miniconda3
+CONDA_BASE=$HOME/ENV/miniconda3
 
 if [[ $OS == "linux" ]];then
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-    bash Miniconda3-latest-Linux-x86_64.sh -b -u -p $PREFIX && \
-    conda init 
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+    bash Miniconda3-latest-Linux-x86_64.sh -b -u -p $CONDA_BASE
+    $CONDA_BASE/bin/conda init
     rm Miniconda3-latest-Linux-x86_64.sh*
 fi
 
