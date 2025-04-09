89fbc581372743fbd4ef08d31bc1ce6f7aae1e68
coolseaweed <jeellato@gmail.com>
2025-04-09 22:46:46 +0900

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 set -e	
 set -o pipefail
 
+DOTFILES=$(realpath ./dot_files)
+
+
+
 cd "$(dirname "${BASH_SOURCE}")";
 
 
@@ -14,6 +18,13 @@ function envSetup() {
     [ -d ~/.vim/bundle ] && rm -rf ~/.vim/bundle
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
+
+
+    # Setup skhd & yabai
+    echo "Symlinking dotfiles"
+    ln -s $DOTFILES/skhdrc.symlink $HOME/.skhdrc
+
+
 }
 
 read -p "Your current setting files (~/.bashrc & ~/.vimrc) will copy with '*.orig' extension in $HOME directory. Are you sure? (y/n) " -n 1;
@@ -21,3 +32,6 @@ echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	envSetup;
 fi; 
+
+
+
